6b6b9381b25759dd22556df9f37b191c168252a2
Archie Baer <archie@abaer.dev>
2020-04-15 01:06:09 +0100

    Formatting

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -3,7 +3,7 @@ import theme from "./theme";
 export default () => (
 	<style jsx global>{`
 		@import url("https://fonts.googleapis.com/css?family=Material+Icons|Rubik:400,500,700,900&display=swap");
-		
+
 		html,
 		body,
 		body > div:first-child,
